f61b420cda4771f5092e6dc6f30973de20b5ce00
XXie91 <47953895+XXie91@users.noreply.github.com>
2019-05-01 20:05:21 +0100

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,17 +1,28 @@
 import os
 import argparse
+import logging
+import time
 import numpy as np
+import numpy.random as npr
+import pandas as pd
 import matplotlib
 matplotlib.use('agg')
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--adjoint', type=eval, default=False)
 parser.add_argument('--visualize', type=eval, default=False)
+parser.add_argument('--niters', type=int, default=2000)
+parser.add_argument('--lr', type=float, default=0.0001)
 parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--train_dir', type=str, default='nnr/')
 args = parser.parse_args()
+
+torch.backends.cudnn.benchmark = True
 
 if args.adjoint:
     from torchdiffeq import odeint_adjoint as odeint
@@ -19,8 +30,8 @@ else:
     from torchdiffeq import odeint
     
 def loadCSVfile2():
-    tmp = np.loadtxt("UK.csv", dtype=np.float, delimiter=",")
-    total_len = 2000
+    tmp = np.loadtxt("UK.csv", dtype=np.str, delimiter=",")
+    total_len = 920
     timestep = 60
     samp_trajs = tmp[0:timestep]
     for num in range(1,total_len):
@@ -79,6 +90,7 @@ class RecognitionRNN(nn.Module):
     def initHidden(self):
         return torch.zeros(self.nbatch, self.nhidden)
 
+
 class Decoder(nn.Module):
 
     def __init__(self, latent_dim=4, obs_dim=2, nhidden=20):
@@ -115,11 +127,15 @@ if __name__ == '__main__':
     rnn_nhidden = 128
     obs_dim = 3
     pred_dim = 1
-    '''
+    flag = 0
+
+    obj_min = 1000000
+    
     device = torch.device('cuda:' + str(args.gpu)
                           if torch.cuda.is_available() else 'cpu')
     '''
-    device = torch.device('cpu')
+    device = torch.device('cpu')'''
+    torch.cuda.empty_cache()
 
     samp_trajs,samp_ts,nbatch,t_len = loadCSVfile2()
     
@@ -130,6 +146,8 @@ if __name__ == '__main__':
     func = LatentODEfunc(latent_dim, nhidden).to(device)
     rec = RecognitionRNN(latent_dim, obs_dim, rnn_nhidden, nbatch).to(device)
     dec = Decoder(latent_dim, pred_dim, nhidden).to(device)
+    params = (list(func.parameters()) + list(dec.parameters()) + list(rec.parameters()))
+    optimizer = optim.Adam(params, lr=args.lr)
     
     if args.train_dir is not None:
         if not os.path.exists(args.train_dir):
@@ -140,30 +158,93 @@ if __name__ == '__main__':
             func.load_state_dict(checkpoint['func_state_dict'])
             rec.load_state_dict(checkpoint['rec_state_dict'])
             dec.load_state_dict(checkpoint['dec_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            samp_trajs = checkpoint['samp_trajs']
+            samp_ts = checkpoint['samp_ts']
             print('Loaded ckpt from {}'.format(ckpt_path))
 
     try:
-        # backward in time to infer q(z_0)
-        h = rec.initHidden().to(device)
-        for t in reversed(range(48)):
-            obs = samp_trajs[:,t,:]               
-            out, h = rec.forward(obs, h)
-        qz0_mean, qz0_logvar = out[:, :latent_dim], out[:, latent_dim:]
-        epsilon = torch.randn(qz0_mean.size()).to(device)
-        z0 = epsilon * torch.exp(qz0_logvar) + qz0_mean
+        for itr in range(1, args.niters + 1):
+            optimizer.zero_grad()
+            # backward in time to infer q(z_0)
+            h = rec.initHidden().to(device)
+            for t in reversed(range(48)):
+                # samp_trajs.size(1)
+                obs = samp_trajs[:,t,:]               
+                out, h = rec.forward(obs, h)
+            qz0_mean, qz0_logvar = out[:, :latent_dim], out[:, latent_dim:]
+            epsilon = torch.randn(qz0_mean.size()).to(device)
+            z0 = epsilon * torch.exp(qz0_logvar) + qz0_mean
 
-        # forward in time and solve ode for reconstructions
-        pred_z = odeint(func, z0, samp_ts).permute(1, 0, 2)
-        pred_x = dec(pred_z)
-        
+            # forward in time and solve ode for reconstructions
+            pred_z = odeint(func, z0, samp_ts).permute(1, 0, 2)
+            pred_x = dec(pred_z)
+
+            # compute loss
+            noise_std_ = torch.ones(pred_x.size()).to(device)
+            noise_logvar = 2. * torch.log(noise_std_).to(device)
+            logpx = log_normal_pdf(
+                torch.reshape(samp_trajs[:,:,0],(nbatch,t_len,1)), pred_x, noise_logvar).sum(-1).sum(-1)
+            pz0_mean = pz0_logvar = torch.zeros(z0.size()).to(device)
+            analytic_kl = normal_kl(qz0_mean, qz0_logvar,
+                                    pz0_mean, pz0_logvar).sum(-1)
+            loss = torch.mean(-logpx + analytic_kl*0.05, dim=0)
+            loss.backward()
+            optimizer.step()
+            
+            print('Iter: {}, running and MSE = {}/-logpx:{}/KL:{}'.format(itr, loss.data,-logpx.sum(-1).data,analytic_kl.sum(-1).sum(-1).data))
+
+            torch.cuda.empty_cache()
+
+            if (loss.data < obj_min) & (flag == 1):
+                obj_min = loss.data
+                ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
+                torch.save({
+                    'func_state_dict': func.state_dict(),
+                    'rec_state_dict': rec.state_dict(),
+                    'dec_state_dict': dec.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'samp_trajs': samp_trajs,
+                    'samp_ts': samp_ts,
+                }, ckpt_path)
+            if flag == 0:
+                flag = 1;
+                obj_min = loss.data
+
+            if itr%50 ==0:
+                torch.cuda.empty_cache()
+            
         tmp=pred_x
-        tmp=tmp.detach().numpy()
+        tmp=tmp.cpu().detach().numpy()
         tmp=np.reshape(tmp,(nbatch,t_len))
         np.savetxt('pred.csv', tmp, delimiter = ',')
         tmp2=samp_trajs
-        tmp2=tmp2.detach().numpy()
+        tmp2=tmp2.cpu().detach().numpy()
         tmp2=np.reshape(tmp2[:,:,0],(nbatch,t_len))
         np.savetxt('ref.csv', tmp2, delimiter = ',')
-        
+        np.savetxt('err.csv', tmp-tmp2, delimiter = ',')
+            
     except KeyboardInterrupt:
         print('Interrupted!')
+    except RuntimeError as e:
+        if 'out of mempry' in str(e):
+            print('Warning: ran out of memory')
+            if hasattr(torch.cuda,'empty_cache'):
+                torch.cuda.empty_cache()
+        else:
+            raise e
+        
+    if args.train_dir is not None:
+        ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
+        '''torch.save({
+            'func_state_dict': func.state_dict(),
+            'rec_state_dict': rec.state_dict(),
+            'dec_state_dict': dec.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'samp_trajs': samp_trajs,
+            'samp_ts': samp_ts,
+        }, ckpt_path)
+        print('Stored ckpt at {}'.format(ckpt_path))'''
+    
+    print('Training complete after {} iters.'.format(itr))
+    
